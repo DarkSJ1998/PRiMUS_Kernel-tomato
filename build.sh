ca7e0c53ca5bbcd26f0351b2c43ceeabe32051ba
DarkSJ1998 <sarthakjn12@gmail.com>
2017-01-15 16:24:09 +0530

    Add build.sh script

---
+++ build.sh
@@ -0,0 +1,98 @@
+#
+ # Copyright © 2014, Varun Chitre "varun.chitre15" <varun.chitre15@gmail.com>
+ # Copyright © 2015, Sanyam Jain "Men_in_black007" <sanyam.53jain@gmail.com>
+ #
+ # Custom build script
+ #
+ # This software is licensed under the terms of the GNU General Public
+ # License version 2, as published by the Free Software Foundation, and
+ # may be copied, distributed, and modified under those terms.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # Please maintain this if you use this script or any part of it
+ #
+KERNEL_DIR=$PWD
+KERN_IMG=$KERNEL_DIR/arch/arm64/boot/Image
+DTBTOOL=$KERNEL_DIR/dtbToolCM
+BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+# Modify the following variable if you want to build
+export CROSS_COMPILE="/home/darksj1998/PRIMUS_V1.0/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+export LD_LIBRARY_PATH=/home/darksj1998/PRIMUS_V1.0/aarch64-linux-android-4.9/lib/
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER="DarkSJ1998"
+export KBUILD_BUILD_HOST="Cybertron"
+STRIP="/home/darksj1998/PRIMUS_V1.0/aarch64-linux-android-4.9/bin/aarch64-linux-android-strip"
+MODULES_DIR=/home/darksj1998/PRIMUS_V1.0/primus/common
+OUT_DIR=/home/darksj1998/PRIMUS_V1.0/primus/tomato
+
+compile_kernel ()
+{
+echo -e "**********************************************************************************************"
+echo "                    "
+echo "                                        Compiling PRiMUS Kernel                    "
+echo "                    "
+echo -e "**********************************************************************************************"
+make primus_tomato-64_defconfig
+make Image -j8
+make dtbs -j8
+make modules -j8
+if ! [ -a $KERN_IMG ];
+then
+echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
+exit 1
+fi
+$DTBTOOL -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
+strip_modules
+}
+
+strip_modules ()
+{
+echo "Copying modules"
+rm $MODULES_DIR/*
+find . -name '*.ko' -exec cp {} $MODULES_DIR/ \;
+cd $MODULES_DIR
+echo "Stripping modules for size"
+$STRIP --strip-unneeded *.ko
+zip -9 modules *
+cd $KERNEL_DIR
+}
+
+case $1 in
+clean)
+make ARCH=arm64 -j3 clean mrproper
+rm -rf $KERNEL_DIR/arch/arm/boot/dt.img
+;;
+*)
+compile_kernel
+;;
+esac
+
+rm -rf $OUT_DIR/PRiMUS_Kernel*.zip
+rm -rf $OUT_DIR/tools/*
+rm -rf $OUT_DIR/system/lib/modules/*
+cp -r $KERNEL_DIR/primus/tools $OUT_DIR
+cp $KERNEL_DIR/arch/arm64/boot/Image  $OUT_DIR/tools/zImage
+cp $KERNEL_DIR/arch/arm64/boot/dt.img  $OUT_DIR/tools
+cp $MODULES_DIR/*.ko $OUT_DIR/system/lib/modules/
+cd $OUT_DIR
+zip -r PRiMUS_Kernel-v1.0-tomato-$(date +"%Y-%m-%d"-%H%M).zip *
+cd $KERNEL_DIR
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+echo -e "**********************************************************************************************"
+echo "                    "
+echo "                                        Enjoy PRiMUS Kernel                    "
+echo "                               PRiMUS_Kernel-v1.0-tomato-$(date +"%Y-%m-%d"-%H%M).zip  " 
+echo " "
+echo -e "**********************************************************************************************"
